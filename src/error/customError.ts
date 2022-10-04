8688f50be751bc408444610061a7a931e49b6508
강진희 <jinheekang@gangjinhuiui-MacBookAir.local>
2022-10-04 22:07:45 +0900

    FIX: customError

--- src/error/customError.ts
+++ src/error/customError.ts
@@ -1,6 +1,8 @@
-// 필수 파라미터 누락
-export const requiredParam = (obj: any, req: any) => {
-  return new Error();
-};
+export class ValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "ValidationError";
+  }
+}
 
-export default { requiredParam };
+export default { ValidationError };
